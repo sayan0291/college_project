11ac064f16c947143c256d20e77bf946e8cbce3b
Soumya <soumyadiphaldar3@gmail.com>
2025-12-05 20:51:51 +0530

    add basic setup

---
+++ server/src/config/cloudinary.js
@@ -0,0 +1,27 @@
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+
+cloudinary.config = {
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+};
+
+const uploadToCloudinary = async(localFilePath) => {
+    if (!localFilePath) {
+        return null;
+    }
+
+    try {
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+        return response;
+    } catch (error) {
+        console.log("Cloudinary connection failed..", error.message);
+    } finally {
+        fs.existsSync(localFilePath) && fs.unlinkSync(localFilePath);
+    }
+}
+
+module.exports = uploadToCloudinary;

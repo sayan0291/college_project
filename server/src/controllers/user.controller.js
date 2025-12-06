4cb1334b72211702717ea0e76f41677d8b8eecf1
Soumya <soumyadiphaldar3@gmail.com>
2025-12-06 21:38:47 +0530

    add user controller

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,11 @@
+const asyncHandler = require("../utils/asyncHandler.js");
+const apiResponse = require("../utils/apiResponse.js");
+const apiError = require("../utils/apiError.js");
+
+const register = asyncHandler(async(req, res) => {
+    
+});
+
+module.exports = {
+    register,
+}

a6a226b7883ea2c49463499a47490896f7828bf7
Soumya <soumyadiphaldar3@gmail.com>
2025-12-13 17:43:08 +0530

    add new features in subject controller..

--- server/src/controllers/subject.controller.js
+++ server/src/controllers/subject.controller.js
@@ -2,6 +2,7 @@ const asyncHandler = require("../utils/asyncHandler.js");
 const apiResponse = require("../utils/apiResponse.js");
 const apiError = require("../utils/apiError.js");
 const Subject = require("../models/subject.model.js");
+const Semester = require("../models/semester.model.js");
 
 const addSubject = asyncHandler(async(req, res) => {
     const { subjectName, departmentId, semesterId } = req.body;
@@ -26,6 +27,90 @@ const addSubject = asyncHandler(async(req, res) => {
     )
 });
 
+const getSubjectBySem = asyncHandler(async(req, res) => {
+    const { semesterId } = req.params;
+
+    if (!semesterId) {
+        throw new apiError(400, "Semester id is required..");
+    }
+
+    const semester = await Semester.findById(semesterId);
+    if (!semester) {
+        throw new apiError(404, "No semester found..");
+    }
+
+    const subjects = await Subject.find({ semester: semesterId });
+    if (!subjects) {
+        throw new apiError(404, "No subjects found..");
+    }
+
+    return res.status(200).json(
+        new apiError(200, subjects, "Subjects fetched successfully..")
+    )
+});
+
+const getSubjectById = asyncHandler(async(req, res) => {
+    const { subjectId } = req.params;
+
+    if (!subjectId) {
+        throw new apiError(400, "Subject id is required..");
+    }
+
+    const subject = await Subject.findById(subjectId);
+    if (!subject) {
+        throw new apiError(404, "No subject found..");
+    }
+
+    return res.status(200).json(
+        new apiError(200, subject, "Subject fetched successfully..")
+    )
+});
+
+const updateSubject = asyncHandler(async(req, res) => {
+    const { subjectName } = req.body;
+    const { subjectId } = req.params;
+
+    if (!subjectId) {
+        throw new apiError(400, "Subject id is required..");
+    }
+
+    const subject = await Subject.findById(subjectId);
+    if (!subject) {
+        throw new apiError(404, "No subject found..");
+    }
+
+    const updateFields = {};
+    if(subjectName) updateFields.subjectName = subjectName;
+
+    const updatedSubject = await Subject.findByIdAndUpdate(
+        subjectId,
+        { $set: updateFields },
+        { new: true }
+    );
+
+    return res.status(200).json(
+        new apiResponse(200, updatedSubject, "Subject updated successfully..")
+    )
+});
+
+const deleteSubject = asyncHandler(async(req, res) => {
+    const { subjectId } = req.params;
+
+    if (!subjectId) {
+        throw new apiError(400, "Subject id is required..");
+    }
+
+    await Subject.findByIdAndDelete(subjectId);
+
+    return res.status(200).json(
+        new apiResponse(200, "Deleted subject successfully..")
+    )
+});
+
 module.exports = {
-    addSubject
+    addSubject,
+    getSubjectBySem,
+    getSubjectById,
+    updateSubject,
+    deleteSubject
 }

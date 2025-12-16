6c54feda78288452bdc75d1c78602da099b4610a
Soumya <soumyadiphaldar3@gmail.com>
2025-12-16 22:58:18 +0530

    fix a little bug

--- server/src/routes/semester.route.js
+++ server/src/routes/semester.route.js
@@ -12,8 +12,8 @@ const isTeacher = require("../middlewares/isTeacher.middleware.js");
 
 router.post("/add", verifyJwt, isTeacher, addSemester);
 router.get("/fetch/:departmentId", verifyJwt, getSemesterByDepartment);
-router.get("/fetch/:semesterId", verifyJwt, getSemesterById);
-router.put("/update", verifyJwt, isTeacher, updateSemester);
-router.delete("/delete", verifyJwt, isTeacher, deleteSemester);
+router.get("/fetch/semester/:semesterId", verifyJwt, getSemesterById);
+router.put("/update/:semesterId", verifyJwt, isTeacher, updateSemester);
+router.delete("/delete/:semesterId", verifyJwt, isTeacher, deleteSemester);
 
 module.exports = router;

c32f6ea408d48ac9dd09b13e711512b94be823d1
Ananya <ananyabera103@gmail.com>
2025-12-20 11:26:08 +0530

    made some changes

--- client/src/SectionHeading.jsx
+++ client/src/SectionHeading.jsx
@@ -1,7 +1,7 @@
 function SectionHeading(section){
     return (
         <div className="relative w-fit mx-7 rounded-tl-md rounded-br-md gradient">
-            <h2 className="text-white font-bold p-3 text-lg uppercase font-serif">
+            <h2 className="text-purple-600 font-bold p-3 text-lg uppercase font-serif">
                 {section.categories}
             </h2>
             <span className="absolute -top-1 -right-1 flex">
--- client/src/styles.jsx
+++ client/src/styles.jsx
@@ -1,3 +1,5 @@
+import { outlinedInputClasses } from "@mui/material/OutlinedInput"
+
 export const h2style = {
     color: "blue",
     fontSize: "1.5rem",
@@ -8,23 +10,25 @@ export const h2style = {
 export const inputstyle = {
     padding: "5px 10px",
     backgroundColor: "white",
-    borderRadius: "6px",
+    borderRadius: "8px",
     border: "1px solid #64B5F6",
-    width: "80%",
-    fontSize:"16px",
+    width: "90%",
+    fontSize:"15px",
     color:"navy",
+    outline:"none",
+    boxShadow: "0 1px 8px rgba(108, 121, 202, 0.573)",
 }
 
 export const buttonstyle = {
     padding: "5px 30px",
-    borderRadius: "5px",
+    borderRadius: "10px",
     color: "white",
 }
 
 export const firstSection = "w-full py-10 flex justify-center font-serif"
-export const secondSection = "px-10 bg-blue-200 flex flex-col gap-5 py-5 w-1/3 formborder"
-export const buttonhover  = "bg-blue-600 hover:bg-blue-500 duration-200"
-export const changehover = "font-bold text-blue-600 hover:text-red-400 duration-300"
+export const secondSection = "px-10 bg-blue-100 flex flex-col gap-5 py-5 w-1/3 formborder"
+export const buttonhover  = "bg-blue-600 hover:bg-indigo-500 duration-200"
+export const changehover = "font-bold text-blue-600 hover:text-indigo-500 duration-300"
 export const formsection = "flex flex-col items-center gap-5"
-export const selectsection = "p-2 bg-blue-300 rounded font-md text-blue-950"
+export const selectsection = "py-2 px-3 bg-stone-100 rounded font-md text-blue-950 outline-none"
 
